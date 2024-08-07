d4ce1b12992187540c88a92a73c68c677b7589d6
arafet <marnissiarafet@gmail.com>
2024-08-07 09:26:04 +0100

    data validation pipline completed

---
+++ src/textsummarization/pipeline/stage_02_data_validation.py
@@ -0,0 +1,14 @@
+from textsummarization.conponents.data_validation import DataValiadtion
+from textsummarization.config.configuration import ConfigurationManager
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_files_exist()
+        
